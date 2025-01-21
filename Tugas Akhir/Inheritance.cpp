5609b5cf82d5df05d98efe31e5d2b9bcf291e145
Rendi Hendra Syahputra <babehndr5@gmail.com>
2025-01-21 22:15:24 +0700

    Tugas akhir

---
+++ Tugas Akhir/Inheritance.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+class Karyawan {
+	public:
+		string nama;
+		int nik, gajiPokok;
+};
+
+class KaryawanTetap: public Karyawan {
+	
+};
+
+class KaryawanHarian: public Karyawan {
+	
+};
