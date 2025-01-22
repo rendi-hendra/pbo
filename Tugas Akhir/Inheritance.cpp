96a8ead6c766bb5a96258c6b3daa5688040d35ca
Rendi Hendra Syahputra <babehndr5@gmail.com>
2025-01-22 20:33:55 +0700

    Update Inheritance.cpp
    Update Class KaryawanTetap & Class KaryawanHarian

--- Tugas Akhir/Inheritance.cpp
+++ Tugas Akhir/Inheritance.cpp
@@ -3,14 +3,66 @@ using namespace std;
 
 class Karyawan {
 	public:
-		string nama;
-		int nik, gajiPokok;
+		string nama, nik; 
+		double gajiPokok;
+
+	    virtual void tampilkanInfo() = 0;
 };
 
 class KaryawanTetap: public Karyawan {
-	
+	public:
+		double tunjangan;
+		
+		double hitungGaji() {
+			return gajiPokok + tunjangan;
+		}
+		
+		void tampilkanInfo() override {
+			cout << "Nama: " << nama << endl;
+			cout << "Nik: " << nik << endl;
+			cout << "Tunjangan: " << tunjangan << endl;
+			cout << "Gaji Pokok: " << gajiPokok << endl;
+			cout << "Total Gaji: " << hitungGaji() << endl;
+		}
 };
 
 class KaryawanHarian: public Karyawan {
-	
+	public:
+		int jumlahHariKerja;
+		double upahPerHari;
+		
+		double hitungGaji() {
+        	return jumlahHariKerja * upahPerHari;
+    	}
+    	
+    	void tampilkanInfo() override {
+			cout << "Nama: " << nama << endl;
+			cout << "Nik: " << nik << endl;
+			cout << "Jumlah Hari Kerja: " << jumlahHariKerja << endl;
+			cout << "Upah Per Hari: " << upahPerHari << endl;
+			cout << "Total Gaji: " << hitungGaji() << endl;
+		}
 };
+
+
+int main() {
+	KaryawanTetap kt;
+	kt.nama = "Rendi";
+	kt.nik = "123";
+	kt.gajiPokok = 20000;
+	kt.tunjangan = 10000;
+	kt.tampilkanInfo();
+	
+	cout << endl;
+	
+	KaryawanHarian kh;
+	kh.nama = "Hendra";
+	kh.nik = "321";
+	kh.gajiPokok= 10000;
+	kh.jumlahHariKerja = 30;
+	kh.upahPerHari= 1000;
+	kh.tampilkanInfo();
+	
+	
+	return 0;
+}
